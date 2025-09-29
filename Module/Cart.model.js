5d62544f68feb3593e53c0980cf4699f2238c8c6
anupam1310 <anupamkaushik3@gmail.com>
2025-09-29 15:06:46 +0530

    models created

--- Module/Cart.model.js
+++
@@ -1,9 +0,0 @@
-import { Schema } from "mongoose";
-
-const cartSchema = new Schema({
-    productId: { type: String, required: true },
-    quantity: { type: Number, required: true, default: 1 },
-});
-
-let CartModel = mongoose.model('Cart', cartSchema);
-export default CartModel;
